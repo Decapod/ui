4d476712dc3823921a2e82b79ca170731053ee81
jameswy <jameswy@localhost>
2010-06-28 10:59:31 -0400

    DECA-123: Made working area full-viewport, and added some breadcrumb and toolbar styling

---
+++ components/calibration/js/CameraMessage.js
@@ -0,0 +1,265 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+/*global jQuery,fluid,decapod*/
+
+var decapod = decapod || {};
+
+(function ($) {
+    
+    var generateCutpoints = function (selectors) {
+        return [
+            {id: "message", selector: selectors.message},
+            {id: "supportedCamerasMessage", selector: selectors.supportedCamerasMessage},
+            {id: "supportedCamerasLink", selector: selectors.supportedCamerasLink},
+            {id: "retryLink", selector: selectors.retryLink},
+            {id: "skipLink", selector: selectors.skipLink},
+            {id: "warning", selector: selectors.warning}
+        ];
+    };
+    
+    var generateTree = function (opts, model) {
+        var tree, skipLink, skipWarning, retryLink;
+        var isError = model.status !== "success";
+        var str = opts.strings;
+        var url = opts.urls;
+        
+        if (isError) {
+            skipLink = "skipErrorLink";
+            skipWarning = "skipWarningError";
+            retryLink = "retryLink";
+            
+            tree = [
+                {
+                    ID: "supportedCamerasMessage",
+                    value: str.supportedCamerasMessage
+                },
+                {
+                    ID: "supportedCamerasLink",
+                    linktext: str.supportedCamerasLink,
+                    target: url.supportedCamerasLink
+                }
+            ];
+        } else {
+            skipLink = "skipSuccessLink";
+            skipWarning = "skipWarningSuccess";
+            retryLink = "continueLink";
+            
+            tree = [];
+        }
+        
+        tree.push({
+                ID: "message",
+                value: str[model.status]
+            });
+        tree.push({
+                ID: "warning",
+                value: str[skipWarning]
+            });
+        tree.push({
+                ID: "retryLink",
+                linktext: str[retryLink],
+                target: url[retryLink]
+            });
+        tree.push({
+                ID: "skipLink",
+                linktext: str[skipLink],
+                target: url[skipLink]
+            });
+            
+        return {
+            children: tree
+        };
+    };
+    
+    var bindEvents = function (that) {
+        that.locate("retryLink").click(function (event) {
+            that.testCameras();
+            event.preventDefault();
+        });
+        
+        that.locate("supportedCamerasLink").click(function (event) {
+            that.showSupportedCameras();
+            event.preventDefault();
+        });
+    };
+    
+    var render = function (that) {
+        var tree = generateTree(that.options, that.model);
+        var opts = {
+            cutpoints: generateCutpoints(that.options.selectors)
+        };
+        
+        if (that.templates) {
+            fluid.reRender(that.templates, that.locate("messageContainer"), tree, opts);
+        } else {
+            that.templates = fluid.selfRender(that.locate("messageContainer"), tree, opts);
+        }
+        
+        if (that.model.status !== "success") {
+            bindEvents(that);
+        }
+        
+        that.events.afterRender.fire(that.model);
+    };
+    
+    var initSubcomponents = function (that) {
+        that.progress = fluid.initSubcomponent(that, "progressMessage", [that.locate("progressContainer"), fluid.COMPONENT_OPTIONS]);
+        that.supportedCameras = fluid.initSubcomponent(that, "supportedCameras", [that.locate("supportedCamerasContainer"), fluid.COMPONENT_OPTIONS]);
+    };
+    
+    var setup = function (that) {
+        that.model = that.options.initialModel;
+        initSubcomponents(that);
+        that.supportedCameras.hide();
+        
+        if (that.model.status) {
+            that.currentStatus();
+        } else {
+            that.testCameras();
+        }
+    };
+    
+    decapod.cameraMessage = function (container, options) {
+        var that = fluid.initView("decapod.cameraMessage", container, options);
+        
+        /**
+         * Shows the progress message
+         */  
+        that.startProgress = function () {
+            that.container.addClass(that.options.styles.showProgress);
+        };
+        
+        /**
+         * Hides the progress message
+         */
+        that.stopProgress = function () {
+            that.container.removeClass(that.options.styles.showProgress);
+        };
+        
+        /**
+         * Shows the set of supported cameras
+         */
+        that.showSupportedCameras = function () {
+            that.supportedCameras.show();
+        };
+        
+        /**
+         * Hides the set of supported cameras
+         */
+        that.hideSupportedCameras = function () {
+            that.supportedCameras.hide();
+        };
+        
+        /**
+         * Updates the model (status of cameras) and renders
+         * out the appropriate message
+         * 
+         * @param {Object} status, the status of the cameras
+         */
+        that.updateStatus = function (status) {
+            that.model.status = status;
+            render(that);
+        };
+        
+        /**
+         * Renders out the appropriate message based on the 
+         * current model (status of cameras)
+         */
+        that.currentStatus = function () {
+            render(that);
+        };
+        
+        /**
+         * Tests the status of the cameras.
+         * The model (status of cameras) will be updated and
+         * and the appropriate message rendered.
+         */
+        that.testCameras = function () {
+            var update = function (result) {
+                that.updateStatus(result.status);
+                that.stopProgress();
+            };
+            
+            that.startProgress();
+            decapod.checkCameras(update, update);
+        };
+        
+        setup(that);
+        
+        return that;
+    };
+    
+    fluid.defaults("decapod.cameraMessage", {
+        
+        progressMessage: {
+            type: "decapod.progressMessage",
+            options: {}
+        },
+        
+        supportedCameras: {
+            type: "decapod.supportedCameras",
+            options: {}
+        },
+        
+        selectors: {
+            supportedCamerasContainer: ".dc-cameraMessage-supportedCameras",
+            progressContainer: ".dc-cameraMessage-progress",
+            messageContainer: ".dc-cameraMessage-messageContainer",
+            message: ".dc-cameraMessage-message",
+            supportedCamerasMessage: ".dc-cameraMessage-requirement",
+            supportedCamerasLink: ".dc-cameraMessage-requirementLink",
+            retryLink: ".dc-cameraMessage-retryLink",
+            skipLink: ".dc-cameraMessage-skipLink",
+            warning: ".dc-cameraMessage-warning"
+        },
+        
+        styles: {
+            showProgress: "ds-cameraMessage-showProgress"
+        },
+        
+        strings: {
+            oneCameraIncompatible: "It seems like you only have one camera connected, and it is incompatible.",
+            oneCameraCompatible: "It seems like you have only one camera connected.",
+            notMatchingOneCompatibleOneNot: "It seems like you have two cameras connected; one is compatible, the other is not.",
+            notMatchingIncompatible: "It seems like you have two cameras connected, buty they are not compatible nor matching.",
+            notMatchingCompatible: "It seems like you have two supported cameras connected, but they are not matching.",
+            incompatible: "It seems like you have two matching cameras connected, but they are not compatible.",
+            noCameras: "It seems like no cameras are connected.",
+            success: "To get the best results, you should run through calibration before capturing",
+            supportedCamerasMessage: "Decapod requires two matching, ",
+            supportedCamerasLink: "supported cameras",
+            retryLink: "Try again",
+            continueLink: "Continue to calibration",
+            skipErrorLink: "Skip camera setup",
+            skipSuccessLink: "Skip calibration",
+            skipWarningError: "(You won't be able to capture)",
+            skipWarningSuccess: "Results may be unpredictable"
+        },
+        
+        events: {
+            afterRender: null
+        },
+        
+        initialModel: {
+            status: null
+        },
+        
+        urls: {
+            retryLink: "#_",
+            continueLink: "#_",
+            supportedCamerasLink: "#_",
+            skipErrorLink: "#_",
+            skipSuccessLink: "#_"
+        }
+    });
+    
+})(jQuery);
---
+++ components/calibration/js/SupportedCameras.js
@@ -0,0 +1,140 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+/*global jQuery,fluid,decapod*/
+
+var decapod = decapod || {};
+
+(function ($) {
+    
+    var generateCutpoints = function (selectors) {
+        return [
+            {id: "label", selector: selectors.label},
+            {id: "manufacturers:", selector: selectors.manufacturers},
+            {id: "manufacturerName", selector: selectors.manufacturerName},
+            {id: "model:", selector: selectors.model}
+        ];
+    };
+    
+    var generateTree = function (model, strings) {
+        var tree = [{
+            ID: "label",
+            value: strings.label
+        }];
+        
+        $.each(model, function (manufacturer, devices) {
+            var manufacturerNodes = fluid.transform(devices, function (device) {
+                return {
+                    ID: "model:",
+                    value: device.deviceName
+                };
+            });
+            
+            manufacturerNodes.push({
+                ID: "manufacturerName",
+                value: manufacturer
+            });
+            
+            tree.push({
+                ID: "manufacturers:",
+                children: manufacturerNodes
+            });
+        });
+            
+        return {
+            children: tree
+        };
+    };
+    
+    var render = function (that) {
+        var tree = generateTree(that.model, that.options.strings);
+        var opts = {
+            cutpoints: generateCutpoints(that.options.selectors)
+        };
+        
+        if (that.templates) {
+            fluid.reRender(that.templates, that.container, tree, opts);
+        } else {
+            that.templates = fluid.selfRender(that.container, tree, opts);
+        }
+        
+        that.locate("closeButton").click(that.hide);
+        
+        that.events.afterRender.fire();
+    };
+    
+    var setup = function (that) {
+        that.model = that.options.model;
+        
+        if (!that.model) {
+            decapod.checkSupportedCameras(function (model) {
+                that.model = model;
+            }, 
+            function () {
+                that.model = {};
+            });
+        }
+        
+        that.refreshView();
+    };
+    
+    decapod.supportedCameras = function (container, options) {
+        var that = fluid.initView("decapod.supportedCameras", container, options);
+        
+        /**
+         * Refreshes the view. 
+         * Useful if the model has been updated.
+         */
+        that.refreshView = function () {
+            render(that);
+        };
+        
+        /**
+         * Shows the entire component
+         */
+        that.show = function () {
+            that.container.show();
+        };
+        
+        /**
+         * Hides the entire component
+         */
+        that.hide = function () {
+            that.container.hide();
+        };
+        
+        setup(that);
+        
+        return that;
+    };
+    
+    fluid.defaults("decapod.supportedCameras", {
+        
+        selectors: {
+            manufacturers: ".dc-supportedCameras-manufacturer",
+            manufacturerName: ".dc-supportedCameras-manufacturerName",
+            model: ".dc-supportedCameras-model",
+            label: ".dc-supportedCameras-label",
+            closeButton: ".dc-supportedCameras-closeButton"
+        },
+        
+        strings: {
+            label: "Supported Cameras"
+        },
+        
+        events: {
+            afterRender: null
+        },
+        
+        model: null
+    });
+    
+})(jQuery);
