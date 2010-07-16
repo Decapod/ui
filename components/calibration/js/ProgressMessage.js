42c969b11123c25de57812ba2ae9d2016dbb84c4
obara justin <obara.justin@gmail.com>
2010-07-16 11:49:13 -0400

    DECA-57: Temporary stop-gap solution for export progress message.
    Hooked into the export progress message that was already in the markup.
    Just hiding and showing when the export button is clicked and the download returned.

---
+++ components/calibration/js/ProgressMessage.js
@@ -0,0 +1,79 @@
+/*
+Copyright 2010 University of Toronto
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+/*global jQuery,fluid,decapod*/
+
+var decapod = decapod || {};
+
+(function ($) {
+    
+    var generateCutpoints = function (options) {
+        var selectors = options.selectors;
+        return [
+            {id: "message", selector: selectors.message},
+            {id: "courtesyMessage", selector: selectors.courtesyMessage},
+            {id: "cancel", selector: selectors.cancel},
+            {id: "cancelInfo", selector: selectors.cancelInfo}
+        ];
+    };
+    
+    var generateTree = function (model, opts) {
+        var tree = [];
+        $.each(opts.strings, function (stringName, string) {
+            tree.push({
+                ID: stringName,
+                value: string
+            });
+        });
+            
+        return {
+            children: tree
+        };
+    };
+    
+    var render = function (that) {
+        decapod.render(that, generateTree, generateCutpoints);
+    };
+    
+    decapod.progressMessage = function (container, options) {
+        var that = fluid.initView("decapod.progressMessage", container, options);
+        
+        that.refreshView = function () {
+            render(that);
+        };
+        
+        that.refreshView();
+        
+        return that;
+    };
+    
+    fluid.defaults("decapod.progressMessage", {
+        
+        selectors: {
+            message: ".dc-progressMessage-message",
+            courtesyMessage: ".dc-progressMessage-courtesyMessage",
+            cancel: ".dc-progressMessage-cancel",
+            cancelInfo: ".dc-progressMessage-cancelInfo"
+        },
+        
+        strings: {
+            message: "Profiling your cameras.",
+            courtesyMessage: "Please wait.",
+            cancel: "Cancel",
+            cancelInfo: "(Go back to Book Management)"
+        },
+        
+        events: {
+            afterRender: null
+        }
+    });
+    
+})(jQuery);
