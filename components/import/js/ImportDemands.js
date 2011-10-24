b0ff8000c7c9ce42b3bded2026ec82f72b48fac3
Justin Obara <obara.justin@gmail.com>
2011-10-24 11:30:15 -0400

    DECA-167: Using IoC to use demo version of uploader when run from the file systyem
    Import will use the demo version of uploader when run locally from the file system, but will use the appropriate server config when run through the server. This is useful for testing out the UI, for example, when tweaking styles.

---
+++ components/import/js/ImportDemands.js
@@ -0,0 +1,38 @@
+/*
+Copyright 2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+// Declare dependencies
+/*global decapod:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+
+    // Run in demo mode if loaded from the local file system
+    fluid.demands("fluid.uploader", ["decapod.fileSystem", "decapod.import"], {
+        options: {
+            demo: true
+        }
+    });
+    
+    fluid.demands("fluid.uploader", ["decapod.import"], {
+        options: {
+            queueSettings: {
+                uploadURL: "/imageImport/"
+            }
+        }
+    });
+    
+})(jQuery);
+
