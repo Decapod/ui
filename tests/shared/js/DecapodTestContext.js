4595dba8fba8125d8a080f1165946ed37caa0594
Justin Obara <obara.justin@gmail.com>
2012-01-17 17:12:01 -0800

    DECA-220: The imageImport url is no longer valid, switched to POSTing to the Pages resource.

---
+++ tests/shared/js/DecapodTestContext.js
@@ -0,0 +1,22 @@
+/*
+Copyright 2011 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+// Declare dependencies
+/*global window, decapod:true, fluid, jQuery, jqUnit*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    fluid.staticEnvironment.decapodTestEnvironment = fluid.typeTag("decapod.test");
+})(jQuery);
