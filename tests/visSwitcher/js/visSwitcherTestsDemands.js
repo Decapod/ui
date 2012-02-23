9f15590e8a38e3344efa28532c1b50543b35d448
Justin Obara <obara.justin@gmail.com>
2012-02-23 03:14:13 -0800

    DECA-218: Implemented a component to adjust the visibility of an element or set of elements. This will be used to hide/show status and instructions, as well as the export control elements.

---
+++ tests/visSwitcher/js/visSwitcherTestsDemands.js
@@ -0,0 +1,28 @@
+/*
+Copyright 2012 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+// Declare dependencies
+/*global decapod:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    
+    /*****************
+     * Event Demands *
+     *****************/
+
+    fluid.demands("testModel", ["decapod.test", "decapod.visSwitcher"], {args: ["{visSwitcher}", "{arguments}.0", "{arguments}.1"]});
+    
+})(jQuery);
