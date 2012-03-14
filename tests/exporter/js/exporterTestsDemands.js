7836e76b18291edb247c13a8d2f505391995c6f0
Justin Obara <obara.justin@gmail.com>
2012-03-13 21:27:38 -0700

    DECA-218: Fixing up the exporter tests after changes to the exportType.js

---
+++ tests/exporter/js/exporterTestsDemands.js
@@ -0,0 +1,34 @@
+/*
+Copyright 2012 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one of these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
+// Declare dependencies
+/*global decapod:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    fluid.demands("decapod.pdfExporter", ["decapod.test", "decapod.exporter"], {
+        options: {
+            listeners: {
+                "onStartExport.triggerExporter": "{exporter}.events.onExportStart.fire"
+            },
+            resources: {
+                template: {
+                    url: "../../../components/exporter/html/pdfExporterTemplate.html"
+                }
+            }
+        }
+    });
+
+})(jQuery);
