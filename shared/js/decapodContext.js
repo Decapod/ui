e22099a010097b280a20bb1e3aa6a86a5905432f
Justin Obara <obara.justin@gmail.com>
2012-04-12 10:01:51 -0700

    DECA-239: Correcting the licenses in all the js files to be apache2

--- shared/js/decapodContext.js
+++ shared/js/decapodContext.js
@@ -1,12 +1,17 @@
 /*
-Copyright 2011 OCAD University
+Copyright 2011 OCAD University 
 
-Licensed under the Educational Community License (ECL), Version 2.0 or the New
-BSD license. You may not use this file except in compliance with one of these
-Licenses.
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
 
-You may obtain a copy of the ECL 2.0 License and BSD License at
-https://source.fluidproject.org/svn/LICENSE.txt
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
 */
 
 // Declare dependencies
