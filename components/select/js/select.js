8fc91914fd277c595604d6fea259f5241cf84f27
Justin Obara <obara.justin@gmail.com>
2012-07-12 12:18:51 -0700

    DECA-275: Added a select component to handle the creation of select boxes. It renders them out and fires events on selection changes. This can also be swapped out in the future for a rich select box.

---
+++ components/select/js/select.js
@@ -0,0 +1,81 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global window, decapod:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+
+    /******************
+     * decapod.select *
+     ******************/
+
+    fluid.registerNamespace("decapod.select");
+    
+    decapod.select.produceTree = function (that) {
+        return {
+            label: {
+                messagekey: "label"
+            },
+            choices: {
+                "selection": "${selection}",
+                "optionlist": "${choices}",
+                "optionnames": "${names}"
+            }
+        };
+    };
+    
+    decapod.select.finalInit = function (that) {
+        that.applier.modelChanged.addListener("*", function (newModel, oldModel) {
+            that.events.afterSelectionChanged.fire(newModel.selection, oldModel.selection);
+        });
+        
+        fluid.fetchResources(that.options.resources, function (resourceSpec) {
+            that.container.append(that.options.resources.template.resourceText);
+            that.events.afterFetchResources.fire(resourceSpec);
+            that.refreshView();
+        });
+    };
+    
+    fluid.defaults("decapod.select", {
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+        finalInitFunction: "decapod.select.finalInit",
+        produceTree: "decapod.select.produceTree",
+        selectors: {
+            label: ".dc-select-label",
+            choices: ".dc-select-choices"
+        },
+        strings: {
+            label: ""
+        },
+        model: {}, // in the form {selection: "", choices: [], names: []}
+        events: {
+            afterFetchResources: null,
+            afterSelectionChanged: null
+        },
+        resources: {
+            template: {
+                url: "../html/selectTemplate.html",
+                forceCache: true
+            }
+        }
+    });
+})(jQuery);
