e9f8653b70b1b49cbdd1e2f73dca6468476077eb
Justin Obara <obara.justin@gmail.com>
2012-07-30 23:37:22 -0700

    DECA-275: Added a new component grade which will allow for the arrays in a model to be replaced instead of merged.

--- tests/exporter/js/exportTypeTests.js
+++ tests/exporter/js/exportTypeTests.js
@@ -109,6 +109,71 @@ var decapod = decapod || {};
     // Tests
     $(document).ready(function () {
 
+        /*********************************
+         * viewComponentCustomMergeTests *
+         *********************************/
+        
+        var viewComponentCustomMergeTests = jqUnit.testCase("decapod.viewComponentCustomMerge");
+        
+        viewComponentCustomMergeTests.test("decapod.viewComponentCustomMerge.extend", function () {
+            
+            var tests = [
+                {name: "Two empty arrays", target: [], source: [], expected: []},
+                {name: "Two empty objects", target: {}, source: {}, expected: {}},
+                {name: "Target empty array, source empty object", target: [], source: {}, expected: {}},
+                {name: "Target empty object, source empty array", target: {}, source: [], expected: {}},
+                {name: "Target array, source object", target: ["a"], source: {a: "a"}, expected: {a: "a"}},
+                {name: "Target object, source array", target: {a: "a"}, source: ["a"], expected: {a: "a", 0: "a"}},
+                {name: "Merge arrays", target: ["a", "b"], source: ["c", "d"], expected: ["c", "d"]},
+                {name: "Merge objects", target: {a: "a", b: "b"}, source: {b: "B", c: "c"}, expected: {a: "a", b: "B", c: "c"}},
+                {name: "Merge multilevel objects", target: {a: {b: "b", c: "c"}}, source: {a: {b: {d: "d"}}}, expected: {a: {b: {d: "d"}, c: "c"}}},
+                {name: "Merge nested arrays", target: {a: ["a", "b"]}, source: {a: ["c", "d"]}, expected: {a: ["c", "d"]}},
+                {name: "Merge object into nested array", target: {a: ["a"]}, source: {a: {b: "b"}}, expected: {a: {b: "b"}}},
+                {name: "Merge array into nested object", target: {a: {b: "b"}}, source: {a: ["a"]}, expected: {a: {b: "b", 0: "a"}}},
+                {name: "Merge undefined", target: {a: "a"}, source: {a: undefined, b: undefined}, expected: {a: "a"}},
+                {name: "Target is undefined", target: undefined, source: {a: "a"}, expected: {a: "a"}},
+                {name: "Source is undefined", target: {a: "a"}, source: undefined, expected: {a: "a"}},
+                {name: "Target and source are undefined", target: undefined, source: undefined, expected: {}},
+                {name: "Target is a string", target: "string", source: {a: "a"}, expected: {a: "a"}},
+                {name: "Source is a string", target: {a: "a"}, source: "string", expected: {a: "a"}},
+                {name: "Target is an array, source is a string", target: ["a"], source: "string", expected: "string"},
+                {name: "Target is a number", target: 5, source: {a: "a"}, expected: {a: "a"}},
+                {name: "Source is a number", target: {a: "a"}, source: 5, expected: {a: "a"}},
+                {name: "Target is an array, source is a number", target: ["a"], source: 5, expected: 5},
+                {name: "Target is null", target: null, source: {a: "a"}, expected: {a: "a"}},
+                {name: "Source is null", target: {a: "a"}, source: null, expected: {a: "a"}},
+                {name: "Target and source are null", target: null, source: null, expected: {}},
+                {name: "Target is a boolean - true", target: true, source: {a: "a"}, expected: {a: "a"}},
+                {name: "Target is a boolean - false", target: false, source: {a: "a"}, expected: {a: "a"}},
+                {name: "Source is a boolean - true", target: {a: "a"}, source: true, expected: {a: "a"}},
+                {name: "Source is a boolean - false", target: {a: "a"}, source: false, expected: {a: "a"}},
+                {name: "Target is an array, source is a boolean - true", target: ["a"], source: true, expected: true},
+                {name: "Target is an array, source is a boolean - false", target: "[a]", source: false, expected: false}
+            ];
+            
+            $.each(tests, function (idx, test) {
+                var merged = decapod.viewComponentCustomMerge.extend(test.target, test.source);
+                jqUnit.assertDeepEq(test.name, test.expected, merged);
+            });
+            
+        });
+        
+        viewComponentCustomMergeTests.test("Override default model", function () {
+            var newModel = {a: "A", b: ["B"]};
+            var originalModel = {a: "a", b: ["c", "d"], e: "e"};
+            var expectedModel = {a: "A", b: ["B"], e: "e"};
+            fluid.defaults("decapod.test.overideModel", {
+                gradeNames: ["decapod.viewComponentCustomMerge", "autoInit"],
+                model: originalModel
+            });
+            
+            var that = decapod.test.overideModel(".dc-viewComponentCustomMerge", {
+                model: fluid.copy(newModel)
+            });
+            
+            jqUnit.assertDeepEq("The model should be overridden.", expectedModel, that.model);
+        });
+
         /*********************
          * exportPollerTests *
          *********************/
