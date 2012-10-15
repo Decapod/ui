bb15fd3f10c30689e85bc798202cd98469877d73
Justin Obara <obara.justin@gmail.com>
2012-10-15 13:33:46 -0400

    DECA-314: the captureReview component now has a model which specifies the captureIndex and an array of captures (urls). These will be rendered out to the dom. The model can be updated view the updateModel function, which will trigger a refreshView on the rendered items. I removed the datasource, as this will be handled by some other component listening to the onDelete event. Unit tests should all be passing.

--- capture/js/captureReviewer.js
+++ capture/js/captureReviewer.js
@@ -30,20 +30,73 @@ var decapod = decapod || {};
     
     fluid.registerNamespace("decapod.captureReviewer");
 
+    decapod.captureReviewer.updateModel = function (that, newModel) {
+        that.applier.requestChange("", newModel);
+    };
+
+    decapod.captureReviewer.produceTree = function (that) {
+        return {
+            captureIndex: {
+                messagekey: "captureIndex",
+                args: [that.model.captureIndex]
+            },
+            del: {
+                messagekey: "del",
+                decorators: [{
+                    type: "jQuery",
+                    func: "click",
+                    args: function () { that.events.onDelete.fire(that.model.captureIndex); }
+                }]
+            },
+            expander: {
+                type: "fluid.renderer.repeat",
+                repeatID: "captures:",
+                controlledBy: "captures",
+                pathAs: "captureInfo",
+                tree: {
+                    captureIMG: {
+                        target: "${{captureInfo}}"
+                    }
+                }
+            }
+        };
+    };
+    
+    decapod.captureReviewer.preInit = function (that) {
+        /*
+         * Work around for FLUID-4709
+         * These methods are overwritten by the framework after initComponent executes.
+         * This preInit function guarantees that functions which forward to the overwritten versions are available during the event binding phase.
+         */
+        // Similar to the comment above but specifically a work around for FLUID-4334
+        that.refreshView = function () {
+            that.renderer.refreshView();
+        };
+        
+        that.updateModel = function (newModel) {
+            that.updateModel(newModel);
+        };
+    };
+
     decapod.captureReviewer.finalInit = function (that) {
+        that.applier.modelChanged.addListener("*", that.events.afterModelChanged.fire);
         that.events.onReady.fire();
     };
     
     fluid.defaults("decapod.captureReviewer", {
-        gradeNames: ["fluid.viewComponent", "autoInit"],
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+        preInitFunction: "decapod.captureReviewer.preInit",
         finalInitFunction: "decapod.captureReviewer.finalInit",
+        produceTree: "decapod.captureReviewer.produceTree",
         selectors: {
             captureIndex: ".dc-captureReviewer-captureIndex",
             del: ".dc-captureReviewer-del",
-            captures: ".dc-captureReviewer-captures"
+            captures: ".dc-captureReviewer-captures",
+            captureIMG: ".dc-captureReviewer-captureIMG"
         },
+        repeatingSelectors: ["captures"],
         strings: {
-            captureIndex: "Capture #%captureIndex",
+            captureIndex: "Capture #%0",
             del: "Delete"
         },
         model: {
@@ -51,12 +104,16 @@ var decapod = decapod || {};
             captures: []
         },
         events: {
-            onReady: null
+            onReady: null,
+            onDelete: null,
+            afterModelChanged: null
         },
-        components: {
-            dataSource: {
-                type: "decapod.dataSource"
-            }
+        listeners: {
+            "afterModelChanged.refreshView": "{captureReviewer}.refreshView"
+        },
+        renderOnInit: true,
+        invokers: {
+            updateModel: "decapod.captureReviewer.updateModel"
         }
     });
 })(jQuery);
--- tests/capture/js/captureReviewerTests.js
+++ tests/capture/js/captureReviewerTests.js
@@ -24,6 +24,17 @@ var decapod = decapod || {};
 
 (function ($) {
     $(document).ready(function () {
+        
+        // helper functions
+        var assertRender = function (that, expectedModel) {
+            jqUnit.assertEquals("The delete text should be rendered", that.options.strings.del, that.locate("del").text());
+            jqUnit.assertEquals("The capture index should be rendered", "Capture #" + expectedModel.captureIndex, that.locate("captureIndex").text());
+            var captures = that.locate("captureIMG");
+            jqUnit.assertEquals("There should be two images rendered", expectedModel.captures.length, captures.length);
+            captures.each(function (idx, elm) {
+                jqUnit.assertEquals("The image at index" + idx + " should have the correct src set", expectedModel.captures[idx], $(elm).attr("src"));
+            });
+        };
 
         /************************
          * captureReviewerTests *
@@ -45,6 +56,72 @@ var decapod = decapod || {};
             decapod.captureReviewer(".dc-captureReviewer", {
                 listeners: {
                     onReady: assertOnReady
+                }
+            });
+        });
+        
+        captureReviewerTests.asyncTest("afterRender", function () {
+            jqUnit.expect(5);
+            var model = {
+                captureIndex: 10,
+                captures: ["http://localhost:8080/test/image1.jpg", "http://localhost:8080/test/image2.jpg"]
+            };
+            var assertAfterRender = function (that) {
+                assertRender(that, model);
+                start();
+            };
+            decapod.captureReviewer(".dc-captureReviewer", {
+                listeners: {
+                    afterRender: assertAfterRender
+                },
+                model: model
+            });
+        });
+        
+        captureReviewerTests.asyncTest("afterModelChanged", function () {
+            jqUnit.expect(7);
+            var model = {
+                captureIndex: 10,
+                captures: ["http://localhost:8080/test/image1.jpg", "http://localhost:8080/test/image2.jpg"]
+            };
+            var assertAfterRender = function (that) {
+                assertRender(that, model);
+                start();
+            };
+            var assertModelChanged = function (that, newModel) {
+                jqUnit.assertDeepEq("The model should be updated", model, that.model);
+                jqUnit.assertDeepEq("The newModel should be passed down", model, newModel);
+                that.events.afterRender.addListener(assertAfterRender);
+            };
+            decapod.captureReviewer(".dc-captureReviewer", {
+                listeners: {
+                    afterModelChanged: {
+                        listener: assertModelChanged,
+                        args: ["{captureReviewer}", "{arguments}.0"],
+                        priority: "first"
+                    },
+                    onReady: {
+                        listener: function (that) {
+                            that.updateModel(model);
+                        },
+                        args: ["{captureReviewer}"]
+                    }
+                }
+            });
+        });
+        
+        captureReviewerTests.asyncTest("onDelete", function () {
+            jqUnit.expect(1);
+            var assertDelete = function () {
+                jqUnit.assertTrue("The onDelete event should have fired", true);
+                start();
+            };
+            decapod.captureReviewer(".dc-captureReviewer", {
+                listeners: {
+                    afterRender: function (that) {
+                        that.locate("del").click();
+                    },
+                    onDelete: assertDelete
                 }
             });
         });
