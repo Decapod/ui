7cb9e126f37d0d11d70086d38ef48318090679f6
Cindy Qi Li <cli@ocadu.ca>
2012-10-15 16:03:53 -0400

    DECA-314: More work on camera controller component. Need to fix unit tests.

---
+++ capture/js/cameraController.js
@@ -0,0 +1,97 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global setTimeout, window, decapod:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+
+    /*****************************
+     *  decapod.cameraController *
+     *****************************/
+    
+    fluid.registerNamespace("decapod.cameraController");
+
+    decapod.cameraController.capture = function (that) {
+        that.dataSource.post();
+    }
+    
+    decapod.cameraController.handleSuccess = function (that, response) {
+        that.events.onCaptureSuccess.fire(response);
+    }
+    
+    decapod.cameraController.handleError = function (that, xhr, response) {
+        that.events.onCaptureError.fire(xhr, response);
+    }
+    
+    decapod.cameraController.finalInit = function (that) {
+        captureButton = that.locate("captureButton");
+        captureButton.attr("role", "button");
+        captureButton.click(function() {
+            decapod.cameraController.capture(that);
+            that.events.onCapture.fire();
+        });
+        
+        that.applier.modelChanged.addListener("buttonEnabled", function (a, b, c) {
+            captureButton.attr("disabled", !that.model.buttonEnabled);
+        });
+        
+        that.events.onReady.fire();
+    };
+    
+    fluid.defaults("decapod.cameraController", {
+        gradeNames: ["fluid.viewComponent", "autoInit"],
+        finalInitFunction: "decapod.cameraController.finalInit",
+        components: {
+            dataSource: {
+                type: "decapod.dataSource",
+                options: {
+                    url: "http://localhost:8081/stereo/capture",
+                    listeners: {
+                        "success.handler": {
+                            listener: "decapod.cameraController.handleSuccess",
+                            args: ["{cameraController}", "{arguments}.0"]
+                        },
+                        "error.handler": {
+                            listener: "decapod.cameraController.handleError",
+                            args: ["{cameraController}", "{arguments}.0", "{arguments}.1"]
+                        }
+                    }
+                }
+            }
+        },
+        model: {
+            buttonEnabled: true
+        },
+        selectors: {
+            captureButton: ".dc-captureButton"
+        },
+        strings: {
+            capture: "Capture"
+        },
+        events: {
+            onReady: null,
+            onCapture: null,
+            onCaptureSuccess: null,
+            onCaptureError: null
+        }
+    });
+})(jQuery);
---
+++ tests/capture/js/cameraControllerTests.js
@@ -0,0 +1,93 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global setTimeout, decapod:true, fluid, jQuery, jqUnit, start*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    $(document).ready(function () {
+
+        /*************************
+         * cameraControllerTests *
+         *************************/
+        
+        var cameraControllerTests = jqUnit.testCase("decapod.cameraController");
+        
+        cameraControllerTests.test("Init", function () {
+            var that = decapod.cameraController(".dc-capture");
+            jqUnit.assertTrue("The component should have initialized", that);
+            jqUnit.assertEquals("The role attribue of the capture button is set properly", that.locate("captureButton").attr("role"), "button");
+        });
+        
+        cameraControllerTests.asyncTest("onReady", function () {
+            jqUnit.expect(1);
+            var assertOnReady = function () {
+                jqUnit.assertTrue("The onReady event should have fired", true);
+                start();
+            };
+            decapod.cameraController(".dc-capture", {
+                listeners: {
+                    onReady: assertOnReady
+                }
+            });
+        });
+
+        cameraControllerTests.test("onCapture", function () {
+        	expect(1);
+            var that = decapod.cameraController(".dc-capture", {
+                listeners: {
+                    onCapture: function() {
+                        jqUnit.assertTrue("The onCapture event should have fired.", true);
+                        start();
+                    }
+                }
+            });
+            that.locate("captureButton").click();
+        });
+        
+//        cameraControllerTests.test("onCaptureSuccess", function () {
+//        	expect(2);
+//        	var expected = {
+//        		    "captureIndex": 1,
+//        		    "captures": ["http://locahost:8081/data/images/image-1_0.jpg", "http://locahost:8081/data/images/image-1_1.jpg"]
+//        		};
+//            var that = decapod.cameraController(".dc-capture", {
+//                listeners: {
+//	                onReady: {
+//		                listener: function(that) {
+//		                    that.locate("captureButton").click();
+//		                },
+//		                args: ["{cameraController}"]
+//		            },
+//                    onCaptureSuccess: function(response) {
+//                        jqUnit.assertTrue("The onCaptureSuccess event should have fired.", true);
+//                        jqUnit.assertDeepEq("The response is expected", response, expected);
+//                        start();
+//                    }
+//                }
+//            });
+//            
+////            that.locate("captureButton").click();
+//            
+//        });
+        
+    });
+})(jQuery);
