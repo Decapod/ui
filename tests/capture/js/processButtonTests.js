b1126fb05af7c713039193b66fee7a9035d46da4
jhung <jhung@localhost>
2012-10-25 12:48:11 -0700

    DECA-333 : Added restart and download icons when PDF export is completed. Also fixed an issue where the button text alignment didn't look right.

---
+++ tests/capture/js/processButtonTests.js
@@ -0,0 +1,150 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global setTimeout, decapod:true, fluid, jQuery, jqUnit, start*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    $(document).ready(function () {
+
+        /*************************
+         * processButtonTests *
+         *************************/
+        
+        var processButtonTests = jqUnit.testCase("decapod.processButton");
+        
+        processButtonTests.test("Init", function () {
+            jqUnit.expect(3);
+            var that = decapod.processButton(".dc-mainPane");
+            jqUnit.assertTrue("The component should have initialized", that);
+            jqUnit.assertEquals("The role attribue of the process button is set properly", that.locate("button").attr("role"), "button");
+            jqUnit.assertEquals("The initial dataSource method should have been set to GET", that.options.dataSourceConfig.method, "get");
+        });
+        
+        processButtonTests.asyncTest("onReady", function () {
+            jqUnit.expect(1);
+            var assertOnReady = function () {
+                jqUnit.assertTrue("The onReady event should have fired", true);
+                start();
+            };
+            decapod.processButton(".dc-mainPane", {
+                listeners: {
+                    onReady: assertOnReady
+                }
+            });
+        });
+
+        processButtonTests.asyncTest("onProcess", function () {
+            jqUnit.expect(3);
+            
+            var that = decapod.processButton(".dc-mainPane", {
+                listeners: {
+                    onProcess: {
+                        listener: function (that) {
+                            jqUnit.assertTrue("The onProcess event should have fired.", true);
+                            
+                            var button = that.locate("button");
+                            jqUnit.assertTrue("The process button should have been disabled.", button.attr("disabled"));
+                            jqUnit.assertEquals("The process state should have been set.", button.html(), that.options.strings.inProcess);
+                            start();
+                        },
+                        args: ["{processButton}"]
+                    }
+                }
+            });
+            
+            var button = that.locate("button");
+            originalButtonHTML = button.html();
+            button.click();
+        });
+        
+        processButtonTests.asyncTest("onProcessSuccess", function () {
+            jqUnit.expect(5);
+            
+            var originalButtonHTML = "I'm a Button";
+            var inProcessHTML = "in Process";
+            var expected = {
+                    "captureIndex": 1,
+                    "captures": ["../../mock-data/images/capture-10_1.jpg", "../../mock-data/images/capture-10_2.jpg"]
+                };
+            
+            var that = decapod.processButton(".dc-mainPane", {
+                dataSourceConfig: {
+                    method: "post"
+                },
+                strings: {
+                    inProcess: inProcessHTML
+                },
+                listeners: {
+                    onProcessSuccess: {
+                        listener: function (that, response) {
+                            jqUnit.assertTrue("The onProcessSuccess event should have fired.", true);
+                            jqUnit.assertDeepEq("The response is expected", response, expected);
+
+                            var button = that.locate("button");
+                            jqUnit.assertFalse("The process button should have been enabled.", button.attr("disabled"));
+                            jqUnit.assertNotEquals("The in process state should have been removed.", button.html(), inProcessHTML);
+                            jqUnit.assertEquals("The original html should have been placed back.", button.html(), originalButtonHTML);
+                            start();
+                        },
+                        args: ["{processButton}", "{arguments}.0"]
+                    }
+                }
+            });
+            
+            var button = that.locate("button");
+            button.html(originalButtonHTML);
+            button.click();
+        });
+        
+        var verifyDisable = function (container, disableFunc) {
+            jqUnit.expect(4);
+            
+            var that = decapod.processButton(container);
+            var button = that.locate("button");
+            
+            jqUnit.assertFalse("The process button is initially enabled.", button.attr("disabled"));
+            jqUnit.assertFalse("The disabled style is not in place initially.", button.hasClass(that.options.styles.disabled));
+            
+            disableFunc(that);
+            
+            jqUnit.assertTrue("The process button should have been disabled.", button.attr("disabled"));
+            jqUnit.assertTrue("The disabled style should have been applied.", button.hasClass(that.options.styles.disabled));
+        };
+        
+        processButtonTests.test("model change on disabled", function () {
+            var disable = function (that) {
+                that.applier.requestChange("disabled", true);
+            };
+            
+            verifyDisable(".dc-mainPane", disable);
+        });
+        
+        processButtonTests.test("updateModel on disabled", function () {
+            var disable = function (that) {
+                that.updateModel({"disabled": true});
+            };
+            
+            verifyDisable(".dc-mainPane", disable);
+        });
+        
+    });
+})(jQuery);
---
+++ tests/export/exporter/js/imageExporterTests.js
@@ -0,0 +1,259 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global decapod:true, fluid, jQuery, jqUnit, start*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    // Container Selectors
+    var IMAGE_EXPORTER_CONTAINER = ".dc-imageExporter";
+    
+    // Template URLs
+    var EXPORT_INFO_TEMPLATE = "../../../../export/components/exporter/html/exportInfoTemplate.html";
+    var CONTROLS_TEMPLATE = "../../../../export/components/exporter/html/exportControlsTemplate.html";
+    var TRIGGER_TEMPLATE = "../../../../export/components/exporter/html/exportControlsTriggerTemplate.html";
+    var PROGRESS_TEMPLATE = "../../../../export/components/exporter/html/exportControlsProgressTemplate.html";
+    var COMPLETE_TEMPLATE = "../../../../export/components/exporter/html/exportControlsCompleteTemplate.html";
+    var IMAGE_EXPORTER_TEMPLATE = "../../../../export/components/exporter/html/imageExporterTemplate.html";
+
+    var createImageExporter = function (container, options) {
+        var opts = {
+            resources: {
+                imageExporterTemplate: {
+                    url: IMAGE_EXPORTER_TEMPLATE,
+                    forceCache: true
+                },
+                exportInfo: {
+                    url: EXPORT_INFO_TEMPLATE,
+                    forceCache: true
+                },
+                controls: {
+                    url: CONTROLS_TEMPLATE,
+                    forceCache: true
+                },
+                trigger: {
+                    url: TRIGGER_TEMPLATE,
+                    forceCache: true
+                },
+                progress: {
+                    url: PROGRESS_TEMPLATE,
+                    forceCache: true
+                },
+                complete: {
+                    url: COMPLETE_TEMPLATE,
+                    forceCache: true
+                }
+            }
+        };
+        fluid.merge("replace", opts, options || {});
+        return fluid.invokeGlobalFunction("decapod.imageExporter", [container, opts]);
+    };
+    
+    // Tests
+    $(document).ready(function () {
+
+        /********************
+         * imageExporterTests *
+         ********************/
+        
+        var imageExporterTests = jqUnit.testCase("decapod.imageExporter");
+        
+        imageExporterTests.asyncTest("Init tests", function () {
+            jqUnit.expect(1);
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    onReady: {
+                        listener: function (that) {
+                            jqUnit.assertTrue("The component should have initialized", true);
+                            start();
+                        },
+                        args: ["{imageExporter}"],
+                        priority: "last"
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("Fetch Resources", function () {
+            jqUnit.expect(6);
+            var assertFetchResources = function (resourceSpec) {
+                $.each(resourceSpec, function (idx, spec) {
+                    jqUnit.assertTrue("The resourceText is filled out for url: " + spec.url, spec.resourceText);
+                });
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterFetchResources: {
+                        listener: assertFetchResources,
+                        priority: "last"
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("Rendering", function () {
+            var assertRendering = function (that) {
+                decapod.testUtils.exportType.assertexportInfoRender(that.exportInfo);
+                decapod.testUtils.exportType.assertShowTriggerControls(that.exportControls);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterRender: {
+                        listener: assertRendering,
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    }
+                }
+            });
+        });
+
+        imageExporterTests.asyncTest("onExportStart event", function () {
+            jqUnit.expect(4);
+            var assertEvent = function (that) {
+                jqUnit.assertTrue("The onExportStart event should have fired", true);
+                decapod.testUtils.exportType.assertShowProgressControls(that.exportControls);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    onExportStart: {
+                        listener: assertEvent,
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    },
+                    afterFetchResources: {
+                        listener: "{imageExporter}.events.onExportStart.fire",
+                        priority: "last"
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("afterExportComplete", function () {
+            jqUnit.expect(6);
+            var assertEvent = function (that) {
+                jqUnit.assertTrue("The afterExportCompleteEvent should have fired", true);
+                decapod.testUtils.exportType.assertShowCompleteControls(that.exportControls);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterExportComplete: {
+                        listener: assertEvent,
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    },
+                    afterFetchResources: {
+                        listener: "{imageExporter}.events.afterExportComplete",
+                        priority: "last"
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("afterExportComplete - pollComplete", function () {
+            jqUnit.expect(6);
+            var assertEvent = function (that) {
+                jqUnit.assertTrue("The afterExportCompleteEvent should have fired", true);
+                decapod.testUtils.exportType.assertShowCompleteControls(that.exportControls);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterExportComplete: {
+                        listener: assertEvent,
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    },
+                    onReady: {
+                        listener: "{imageExporter}.exportPoller.events.pollComplete",
+                        priority: "last"
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("afterExportComplete - dataSource success", function () {
+            jqUnit.expect(6);
+            var assertEvent = function (that) {
+                jqUnit.assertTrue("The afterExportCompleteEvent should have fired", true);
+                decapod.testUtils.exportType.assertShowCompleteControls(that.exportControls);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterExportComplete: {
+                        listener: assertEvent,
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    },
+                    onReady: {
+                        listener: "{imageExporter}.dataSource.events.success",
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("afterExportComplete - onExportStart", function () {
+            jqUnit.expect(7);
+            var assertEvent = function (that, response) {
+                jqUnit.assertTrue("The afterExportCompleteEvent should have fired", true);
+                jqUnit.assertEquals("The decapod.exportControls.complete model should be updated", response.url, that.exportControls["**-renderer-complete-0"].model.downloadURL);
+                decapod.testUtils.exportType.assertShowCompleteControls(that.exportControls);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterExportComplete: {
+                        listener: assertEvent,
+                        priority: "last",
+                        args: ["{imageExporter}", "{arguments}.0"]
+                    },
+                    onReady: {
+                        listener: "{imageExporter}.events.onExportStart",
+                        priority: "last",
+                        args: ["{imageExporter}"]
+                    }
+                }
+            });
+        });
+        
+        imageExporterTests.asyncTest("afterRender", function () {
+            jqUnit.expect(1);
+            var assertEvent = function (that, response) {
+                jqUnit.assertTrue("The afterRender event should have fired", true);
+                start();
+            };
+            createImageExporter(IMAGE_EXPORTER_CONTAINER, {
+                listeners: {
+                    afterRender: {
+                        listener: assertEvent,
+                        priority: "last"
+                    }
+                }
+            });
+        });
+    });
+})(jQuery);
