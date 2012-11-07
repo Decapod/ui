0fc19d7abe6f1451b17b6ef78c39553091e54836
Yura Zenevich <yura.zenevich@gmail.com>
2012-11-07 15:48:06 -0500

    DECA-320, DECA-337: Moved button to core. Moved stereo component related code to core since it's going to be used by both calibration and dewarping.

---
+++ core/js/stereo.js
@@ -0,0 +1,74 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global setTimeout, window, decapod:true, fluid, jQuery*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+
+    /*********************
+     *  decapod.calibrator *
+     *********************/
+
+     fluid.defaults("decapod.stereo", {
+         gradeNames: ["autoInit", "fluid.rendererComponent"],
+         finalInitFunction: "decapod.stereo.finalInit",
+         selectors: {
+             title: ".dc-stereo-title",
+             help: ".dc-stereo-help"
+         },
+         protoTree: {
+             title: {
+                 messagekey: "title"
+             },
+             help: {
+                 target: "${help}",
+                 linktext: {
+                     messagekey: "help"
+                 }
+             }
+         },
+         model: {
+             help: "#"
+         },
+         strings: {
+             help: "Help"
+         },
+         resources: {
+             template: {
+                 url: "../../core/html/stereoTemplate.html",
+                 forceCache: true,
+                 options: {
+                     dataType: "html"
+                 }
+             }
+         }
+     });
+
+     decapod.stereo.finalInit = function (that) {
+         decapod.fetchResources(that.options.resources, function (resourceSpec) {
+            that.refreshView();
+        });
+     };
+
+    fluid.fetchResources.primeCacheFromResources("decapod.stereo");
+
+})(jQuery);
