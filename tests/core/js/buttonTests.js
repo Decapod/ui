3fced3ca04d44bf6b254dc06d44d298d002f08ac
jhung <jhung@localhost>
2012-11-26 06:34:20 -0800

    DECA-322: Editing capture help docs. Added diagrams.

---
+++ tests/core/js/buttonTests.js
@@ -0,0 +1,178 @@
+/*
+Copyright 2012 OCAD University 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+   http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+// Declare dependencies
+/*global setTimeout, decapod:true, fluid, jQuery, jqUnit, start*/
+
+// JSLint options 
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var decapod = decapod || {};
+
+(function ($) {
+    $(document).ready(function () {
+
+        /***************
+         * buttonTests *
+         ***************/
+        
+        var CONTAINER = ".dc-capturer-button";
+        
+        var assertDisabled = function (that) {
+            jqUnit.assertTrue("The afterDisabled event should have fired", true);
+            jqUnit.assertTrue("The disabled style should have been applied", $(CONTAINER).hasClass(that.options.styles.disabled));
+            jqUnit.assertTrue("The aria-disabled attribute should have been applied", $(CONTAINER).attr("aria-disabled"));
+            jqUnit.assertTrue("The disabled attribute should have been applied", $(CONTAINER).attr("disabled"));
+        };
+        
+        var assertEnabled = function (that) {
+            jqUnit.assertTrue("The afterEnabled event should have fired", true);
+            jqUnit.assertFalse("The disabled style should have not been applied", $(CONTAINER).hasClass(that.options.styles.disabled));
+            jqUnit.assertFalse("The aria-disabled attribute should have not been applied", $(CONTAINER).attr("aria-disabled"));
+            jqUnit.assertFalse("The disabled attribute should have not been applied", $(CONTAINER).attr("disabled"));
+        };
+        
+        var buttonTests = jqUnit.testCase("decapod.button");
+        
+        buttonTests.test("Init", function () {
+            jqUnit.expect(4);
+            var that = decapod.button(CONTAINER);
+            jqUnit.assertTrue("The component should have initialized", that);
+            jqUnit.assertEquals("The role attribue of the process button is set properly", $(CONTAINER).attr("role"), "button");
+            jqUnit.assertEquals("The tabindex should be set", "0", $(CONTAINER).attr("tabindex"));
+            jqUnit.assertEquals("The initial button text should have been set", $(CONTAINER).text(), that.options.strings.label);
+        });
+       
+        buttonTests.asyncTest("Init with enabled state", function () {
+            jqUnit.expect(4);
+            var assertAfterEnabled = function (that) {
+                assertEnabled(that);
+                start();
+            };
+            decapod.button(CONTAINER, {
+                listeners: {
+                    afterEnabled: {
+                        listener: assertAfterEnabled,
+                        args: ["{button}"]
+                    }
+                },
+                model: {
+                    "state": "enabled"
+                }
+            });
+        });
+
+        buttonTests.asyncTest("Init with disabled state", function () {
+            jqUnit.expect(4);
+            var assertAfterDisabled = function (that) {
+                assertDisabled(that);
+                start();
+            };
+            decapod.button(CONTAINER, {
+                listeners: {
+                    afterDisabled: {
+                        listener: assertAfterDisabled,
+                        args: ["{button}"]
+                    }
+                },
+                model: {
+                    "state": "disabled"
+                }
+            });
+        });
+
+        buttonTests.asyncTest("onClick with enabled state", function () {
+            jqUnit.expect(1);
+            var assertOnClick = function (that) {
+                jqUnit.assertTrue("The onClick event should have been fired", true);
+                start();
+            };
+            decapod.button(CONTAINER, {
+                model: {
+                    "state": "enabled"
+                },
+                listeners: {
+                    onClick: assertOnClick,
+                    afterEnabled: function () {
+                        $(CONTAINER).click();
+                    }
+                }
+            });
+        });
+
+        buttonTests.test("onClick with disabled state", function () {
+            jqUnit.expect(1);
+            var onClickFired = false;
+            var assertOnClick = function (that) {
+                onClickFired = true;
+            };
+            decapod.button(CONTAINER, {
+                model: {
+                    "state": "disabled"
+                },
+                listeners: {
+                    onClick: assertOnClick
+                }
+            });
+           
+            $(CONTAINER).click();
+            jqUnit.assertFalse("The onClick event should have not been fired", onClickFired);
+        });
+
+        buttonTests.asyncTest("setState - disabled", function () {
+            jqUnit.expect(4);
+            var assertAfterDisabled = function (that) {
+                assertDisabled(that);
+                start();
+            };
+            var that = decapod.button(CONTAINER, {
+                listeners: {
+                    afterDisabled: {
+                        listener: assertAfterDisabled,
+                        args: ["{button}"]
+                    }
+                },
+                model: {
+                    "state": "enabled"
+                }
+            });
+            
+            that.setState("disabled");
+        });
+        
+        buttonTests.asyncTest("setState - enabled", function () {
+            jqUnit.expect(4);
+            var assertAfterEnabled = function (that) {
+                assertEnabled(that);
+                start();
+            };
+            var that = decapod.button(CONTAINER, {
+                listeners: {
+                    afterEnabled: {
+                        listener: assertAfterEnabled,
+                        args: ["{button}"]
+                    }
+                },
+                model: {
+                    "state": "disabled"
+                }
+            });
+            
+            that.setState("enabled");
+        });
+
+    });
+})(jQuery);
