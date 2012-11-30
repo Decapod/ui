d73201e09a3b07af8bf69f6d9b532e5eaa5ad330
Yura Zenevich <yura.zenevich@gmail.com>
2012-11-30 16:40:06 -0500

    NOJIRA: Updated copyright file header.

--- tests/utils/js/decapodTestContext.js
+++ tests/utils/js/decapodTestContext.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2011 OCAD University 
+Copyright 2011-2012 OCAD University 
 
 Licensed under the Apache License, Version 2.0 (the "License"); 
 you may not use this file except in compliance with the License. 
